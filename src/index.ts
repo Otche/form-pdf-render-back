fdcbe78417e56f4e981f419bc030d31a95a33837
Amine OUCHIHA <ouchihaamine@gmail.com>
2021-01-24 22:03:35 +0100

    feat(init): put typescript as dependency

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ app.get('/', async (request, response) => {
     const firstname= request.query.firstname as string;
     const lastname= request.query.lastname as string;
     const doc =  React.createElement(MyDocument,{firstname: firstname, lastname : lastname});
-    const pdfstream = (await ReactPDF.renderToStream(doc as ReactElement));  
+    const pdfstream = (await ReactPDF.renderToStream(doc as ReactElement)); 
+    console.log("111") 
     pdfstream.pipe(response);
     pdfstream.on('end',() => {
         response.end();
